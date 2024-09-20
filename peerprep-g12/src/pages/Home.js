eba3b2bc59b1d911eb7bd441a5538ad8bd092d0e
Song Fang Yi <songfangyi1210@gmail.com>
2024-09-20 16:20:39 +0800

    Add logout function

--- peerprep-g12/src/pages/Home.js
+++ peerprep-g12/src/pages/Home.js
@@ -1,10 +1,12 @@
 // src/pages/Home.js
 import React, { useContext } from 'react';
+import LogoutButton from '../components/LogoutButton';
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Home Page!</h1>
+      <LogoutButton />
     </div>
   );
 };
