b81ca15c2e210983654af0e64368fbedd14b77a5
Song Fang Yi <songfangyi1210@gmail.com>
2024-09-20 17:29:54 +0800

    Add user service api and create signup function

--- peerprep-g12/src/App.js
+++ peerprep-g12/src/App.js
@@ -1,14 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/Home";
 import ProtectedRoute from "./components/ProtectedRoute";
+import SignupPage from "./pages/SignupPage";
 
 const App = () => {
+
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage/>}/>
         <Route path="/home" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+        <Route path="/" element={<Navigate to={ "/home" } />} />
       </Routes>
     </Router>
   );
