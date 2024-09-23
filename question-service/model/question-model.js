b3e5f1d58aac0329fd6a9d81f33093e1ba2f1f09
TehOPanas <e0960514@u.nus.edu.sg>
2024-09-23 13:17:59 +0800

    Define Question model.

--- question-service/model/question-model.js
+++ question-service/model/question-model.js
@@ -1,12 +1,23 @@
 import mongoose from 'mongoose';
 
-// Define the question schema
-const questionSchema = new mongoose.Schema({
-    questionTitle: { type: String, required: true },
-    questionDescription: String,
-    questionCategory: String,
-    questionComplexity: String
+const {Schema} = mongoose;
+
+const questionExampleSchema = new Schema({
+    input: {type:String, required:true},
+    output:{type:String, required:true}
 });
 
-// Export the model as default
+// Define the question schema
+const questionSchema = new Schema({
+    title:{type: String, required: true},
+    description:{type: String, required:true},
+    hint:{type:String},
+    difficulty:{type:String, required:true},
+    tags: {type:[String]},
+    examples: [questionExampleSchema]
+});
+
 export default mongoose.model('Question', questionSchema);
+
+
+// Export the model as default
