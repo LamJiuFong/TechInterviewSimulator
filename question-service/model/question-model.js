e10f9b84ef213731a2ee78c9c91ba336234fe07f
TehOPanas <e0960514@u.nus.edu.sg>
2024-09-23 15:41:22 +0800

    Add QuestionCategory schema and modify Question schema

--- question-service/model/question-model.js
+++ question-service/model/question-model.js
@@ -1,23 +1,34 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo, Mongoose } from 'mongoose';
 
 const {Schema} = mongoose;
 
+// Schema to represent example present in a question
 const questionExampleSchema = new Schema({
     input: {type:String, required:true},
     output:{type:String, required:true}
 });
 
-// Define the question schema
+// Schema to represent question category (e.g. DP, Array, Divide&Conquer etc.)
+const questionCategorySchema = new Schema({
+    sortCode : {type:Number, required:true},
+    name: {type:String, required:true}
+});
+
+const QuestionCategory = mongoose.model('QuestionCategory', questionCategorySchema);
+
+// Schema to represent Question, Question can be queried w/ categories or/and difficulty.
 const questionSchema = new Schema({
     title:{type: String, required: true},
     description:{type: String, required:true},
     hint:{type:String},
-    difficulty:{type:String, required:true},
-    tags: {type:[String]},
+    difficulty:{type:int, required:true}, // 0 : easy, 1: medium, 2: difficult
+    categories: [{type:Schema.Types.ObjectId, ref:"QuestionCategory"}],
     examples: [questionExampleSchema]
 });
 
-export default mongoose.model('Question', questionSchema);
+const Question = mongoose.model('Question', questionSchema);
+
+module.exports = {Question, QuestionCategory}
 
 
 // Export the model as default
