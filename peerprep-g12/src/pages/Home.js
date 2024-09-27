689d51aa2c8c0c57d2449389d699cc8289908d1d
jingting1412 <jingting.sg@gmail.com>
2024-09-27 20:07:32 +0800

    Add button to question page on home page

--- peerprep-g12/src/pages/Home.js
+++ peerprep-g12/src/pages/Home.js
@@ -1,11 +1,19 @@
 // src/pages/Home.js
 import React from 'react';
 import LogoutButton from '../components/LogoutButton';
+import { Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Home Page!</h1>
+      <Button>
+        <Link to={'/admin/questions'}>
+            View all questions
+        </Link>
+      </Button>
       <LogoutButton />
     </div>
   );
