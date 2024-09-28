b0bab45371173b74954785698837eab9caa0edcd
Lam Jiu Fong <e0960521@u.nus.edu>
2024-09-28 12:15:39 +0800

    Sync APIs

--- question-service/routes/question-route.js
+++ question-service/routes/question-route.js
@@ -1,32 +1,21 @@
 import express from 'express';
-import {
-  getAllQuestions,
-  createNewQuestion,
-  getQuestionById,
-  getQuestionByTitle,
-  updateQuestion,
-  deleteQuestion
-} from '../controller/question-controller.js';  // Import the controller functions
-
-import { getAllQuestions, findQuestionById, postQuestion } from '../controller/question-controller.js'; // Use `import` and .js extension for ES modules
+import { createQuestion, getAllQuestions, getQuestionById, updateQuestionById, deleteQuestionById } from '../controller/question-controller.js'; // Use `import` and .js extension for ES modules
 
 const router = express.Router();
 
 // Define the routes
+router.post("/", createQuestion);
+
 router.get('/', getAllQuestions);
-router.get('/:id', getQuestionById);
+
+router.get("/:id", getQuestionById);
+
+router.put('/:id', updateQuestionById);
+
+router.delete('/:id', deleteQuestionById);
+
+/** Not supported yet
 router.get('/search/title', getQuestionByTitle);
-
-/**
-router.post('/', createNewQuestion);
-
-router.put('/:id', updateQuestion);
-
-router.delete('/:id', deleteQuestion);
 */
-
-router.get("/:questionId", findQuestionById);
-
-router.post("/", postQuestion); // create question
 
 export default router;
