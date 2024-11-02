ab60d0f81ed4075ca746b51c9f3db937e80ef08a
jingting1412 <jingting.sg@gmail.com>
2024-11-02 14:54:50 +0800

    Fix button issue

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,21 @@
 // src/pages/Home.js
 import React from 'react';
 import LogoutButton from '../components/LogoutButton';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 
 const Home = () => {
+  const nav = useNavigate();
+
   return (
     <div>
       <h1>Welcome to Home Page!</h1>
-      <Button class="button">
-        <Link to={'/admin/questions'}>
-            View all questions
-        </Link>
+      <Button class="button" onClick={() => nav('/admin/questions')}>
+          View all questions
       </Button>
-      <Button class="button">
-        <Link to={'/waiting-room'}>
-            Start a new session
-        </Link>
+      <Button class="button" onClick={() => nav('/waiting-room')}>
+          Start a new session
       </Button>
       <LogoutButton />
     </div>
