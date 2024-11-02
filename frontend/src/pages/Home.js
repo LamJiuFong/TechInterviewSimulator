9112e37f1633765afead9136f03cd3c495b0a70e
jingting1412 <jingting.sg@gmail.com>
2024-11-02 16:14:42 +0800

    Update home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,23 +1,34 @@
 // src/pages/Home.js
+import './page-styles/Home.css';
 import React from 'react';
 import LogoutButton from '../components/LogoutButton';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
-
+import { useAuth } from '../context/AuthContext';
+import CreateIcon from '@mui/icons-material/Create';
+import QuizIcon from '@mui/icons-material/Quiz';
 
 const Home = () => {
   const nav = useNavigate();
+  const { user } = useAuth();
+  const username = user.username;
 
   return (
-    <div>
-      <h1>Welcome to Home Page!</h1>
-      <Button class="button" onClick={() => nav('/admin/questions')}>
-          View all questions
-      </Button>
-      <Button class="button" onClick={() => nav('/waiting-room')}>
-          Start a new session
-      </Button>
-      <LogoutButton />
+    <div className='home-page'>
+      <div className='welcome-title'>
+        <h1>Welcome back {username}!</h1>
+        <LogoutButton />
+      </div>
+      <div className='home-actions'>
+        <Button class="session-button" onClick={() => nav('/waiting-room')}>
+          <CreateIcon sx={{ fontSize: 35 }}/>
+          <h2>Start a new session</h2>
+        </Button>
+        <Button class="question-button" onClick={() => nav('/admin/questions')}>
+          <QuizIcon sx={{ fontSize: 30 }}/>
+          <h2>View all questions</h2>
+        </Button>
+      </div>
     </div>
   );
 };
